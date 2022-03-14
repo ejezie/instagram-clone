b47317517ad8e02b99daa0790d23f7570c61234f
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-03-14 17:26:39 +0100

    created signup page auth with firebase

--- App.js
+++ App.js
@@ -1,15 +1,35 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import LandingScreen from './components/auth/Landing';
+import Landing from './components/auth/Landing';
+import Register from './components/auth/Register';
+import { ENV_VAR } from "@env" 
 import 'react-native-gesture-handler';
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBZNwzJ06cJUM88JbbzIrpV-ge0sP7m3AY",
+  authDomain: "instagram-clone-54632.firebaseapp.com",
+  projectId: "instagram-clone-54632",
+  storageBucket: "instagram-clone-54632.appspot.com",
+  messagingSenderId: "999824101611",
+  appId: "1:999824101611:web:78e80d63a03b13b7756eae",
+  measurementId: "G-CSQEGWXF8R"
+};
+
+console.log(ENV_VAR.apiKey )
+
+if(firebase.apps.length === 0){
+  firebase.initializeApp(firebaseConfig);
+}
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='LandingScreen'>
-         <Stack.Screen name='LandingScreen' component={LandingScreen} options={{headerShown: false}}/>
+      <Stack.Navigator initialRouteName='Landing'>
+         <Stack.Screen name='Landing' component={Landing} options={{headerShown: false}}/>
+         <Stack.Screen name='Register' component={Register}/>
       </Stack.Navigator>    
     </NavigationContainer>
   );
--- components/auth/Register.js
+++ components/auth/Register.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react'
 import {View, Button, TextInput} from "react-native"
+import firebase from 'firebase';
 
 export class Register extends Component {
+    constructor(props) {
+        super();
+
+        this.state = {
+            email: "",
+            password: "",
+            name: "",
+        }
+
+        this.onSignUp = this.onSignUp.bind(this); 
+    }
+
+    onSignUp(){
+        const {email, password, name} = this.state
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then((response)=>{
+            console.log(response);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
   render() {
     return (
-      <View>Register</View>
+      <View>
+          <TextInput placeholder='name' onChangeText={(name) => this.setState({name})}/>
+          <TextInput placeholder='email' onChangeText={(email) => this.setState({email})}/>
+          <TextInput secureTextEntry={true} placeholder='password' onChangeText={(password) => this.setState({password})}/>
+          <Button onPress={()=> this.onSignUp()} title={"Sign Up"}/>
+      </View>
     )
   }
 }

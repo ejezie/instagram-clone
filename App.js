17248a7075ef5a7c9d047effef2ac04be119ca49
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-03-14 13:03:30 +0100

    added the landing page

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LandingScreen from './components/auth/Landing';
+import 'react-native-gesture-handler';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='LandingScreen'>
+         <Stack.Screen name='LandingScreen' component={LandingScreen} options={{headerShown: false}}/>
+      </Stack.Navigator>    
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

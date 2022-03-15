2db4a4481f4962e595e664925e089af198caca8d
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-03-15 13:46:52 +0100

    redux setup

---
+++ components/Main.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class Main extends Component {
+
+  componentDidMount(){
+
+    }
+
+  render() {
+    return (
+      <div>Main</div>
+    )
+  }
+}
+
+export default Main
